5857689f8c7e33391c83237881a5f4b0d5409a45
Mohd Siraj <siraj.mohd@tarento.com>
2024-09-27 15:16:57 +0530

    main branch - initial commit

---
+++ src/components/Credit.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Credit() {
+  return (
+    <div class="credit">
+    <p>
+      Coded by
+      <a href="https://media.geeksforgeeks.org/courses/certificates/b6614e142bbd6d7c1f3374eb06854a16.pdf"
+        > Mohd Siraj</a
+      >, <a href="https://github.com/Mohd-Siraj/weatherapp">Open sourced on Github.</a> Hosted on Netlify
+    </p>
+  </div>
+  );
+}
